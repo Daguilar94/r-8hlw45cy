e26492c3f04fff5928e4a7fd139d1f9f58d8df74
David Aguilar <davidaguilarh.94@gmail.com>
2018-01-08 03:02:46 -0500

    Fix visibility

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.acceptTerms = this.acceptTerms.bind(this)
+    this.state = {
+      check: false
+    }
+  }
   render() {
     return (
       <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <label><input type="checkbox" checked={this.state.check} onChange={this.acceptTerms}/> Mostrar información importante</label>
+        {this.state.check && <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> }
+
       </div>
     );
+  }
+
+  acceptTerms(e) {
+    this.setState({
+      check: e.target.checked
+    })
   }
 }
 
